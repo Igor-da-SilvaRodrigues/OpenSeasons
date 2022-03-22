bad30f68061af1dc592f08cfa028c12e44b5a03d
Igor-da-SilvaRodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-03-22 17:06:19 -0300

    Create OpenSeasonsWorldState.java

---
+++ src/main/java/openseasons/OpenSeasonsWorldState.java
@@ -0,0 +1,15 @@
+package openseasons;
+
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.PersistentState;
+
+public class OpenSeasonsWorldState extends PersistentState {
+
+
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt) {
+
+        return null;
+    }
+}
